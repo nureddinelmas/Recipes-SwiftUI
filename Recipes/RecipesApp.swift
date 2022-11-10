c1ac4cdfaf57911a52550f8b403c3a6edf393e94
nureddinelmas <nureddinelmas34@gmail.com>
2022-11-10 21:33:49 +0100

    Initial Commit

---
+++ Recipes/RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesApp.swift
+//  Recipes
+//
+//  Created by Nureddin Elmas on 2022-11-10.
+//
+
+import SwiftUI
+
+@main
+struct RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
